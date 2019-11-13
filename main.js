bdf3314e1c3c4107f1087f068b7f38d9545ecbf4
artur.stanaszek <arturstan@gmail.com>
2019-11-13 13:40:10 +0100

    BEM

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-alert("hej!");
+// alert("hej!");
 console.log("hej konsola");
+
+
+const fillJS = document.querySelector('.paragraph--js');
+// console.log(fillJS);
+fillJS.innerHTML = "Testuję wypełnianie przez JS";
