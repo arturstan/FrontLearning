b5fc6cc870d440e0c37471dc8ee7f5525fd9a0b7
artur.stanaszek <arturstan@gmail.com>
2019-11-13 14:38:45 +0100

    js - example function

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ console.log("hej konsola");
 const fillJS = document.querySelector('.paragraph--js');
 // console.log(fillJS);
 fillJS.innerHTML = "Testuję wypełnianie przez JS";
+
+console.log(imieNazwisko('Artur', 'Stanaszek'));
+
+function imieNazwisko(imie, nazwisko){
+	return imie + ' ' + nazwisko;
+}
